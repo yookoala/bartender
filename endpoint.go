91979419618f81eed91f54a3cb2b8dcde3379e4a
Koala Yeung <koalay@gmail.com>
2015-09-03 23:30:25 +0800

    Endpoint() creates a passby Endpoint{}
    bartender.Endpoint() returns a passby Endpoint no matter what the
    input is.

--- endpoint.go
+++ endpoint.go
@@ -5,7 +5,6 @@ import (
 	"reflect"
 
 	"github.com/go-kit/kit/endpoint"
-	"golang.org/x/net/context"
 )
 
 func isFunc(v interface{}) bool {
@@ -15,7 +14,6 @@ func isFunc(v interface{}) bool {
 // Endpoint wraps a provided function and returns
 // a valid go-kit Endpoint
 func Endpoint(fn interface{}) (e endpoint.Endpoint, err error) {
-
 	if !isFunc(fn) {
 		err = fmt.Errorf("input is not a function: %#v", fn)
 		return
@@ -32,11 +30,35 @@ func Endpoint(fn interface{}) (e endpoint.Endpoint, err error) {
 		// TODO: might accept lesser return parameter (e.g. no error or not output)
 	}
 
-	// TODO: parse the fn for later `req` checking
+	// TODO: rewrite `pass` into `call`
+	// -> to examine if the input values type
+	//    and return error if mismatch
+	// -> to call fn with input values,
+	//    then return the output to the output values
+	pass := func(in []reflect.Value) []reflect.Value {
 
-	e = func(ctx context.Context, req interface{}) (resp interface{}, err error) {
-		resp = 8
-		return
+		// temp: construct an empty error variable
+		// and return its value
+		var errVar error
+		err := reflect.ValueOf(&errVar).Elem()
+
+		return []reflect.Value{in[1], err} // pass the request to response
 	}
+
+	makeEndpoint := func(fin, fpout interface{}) {
+		// fptr is a pointer to a function.
+		// Obtain the function value itself (likely nil) as a reflect.Value
+		// so that we can query its type and then set the value.
+		fn := reflect.ValueOf(fpout).Elem()
+
+		// Make a function of the right type.
+		v := reflect.MakeFunc(fn.Type(), pass)
+
+		// Assign it to the value fn represents.
+		fn.Set(v)
+	}
+
+	makeEndpoint(fn, &e)
+
 	return
 }
